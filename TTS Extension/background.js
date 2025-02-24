fbf5f3a3cd6bbb9d46101d36f46d2cc1926f5e4a
Nour-Saeed01 <138711378+Nour-Saeed01@users.noreply.github.com>
2025-02-24 16:42:28 +0200

    Add files via upload
    The extension is now a side panel
    Activity timer is working (15 secs but we can change it)
    Added more datasets (exercises,.. etc), and they are working well, but we might need to modify them later

--- TTS Extension/background.js
+++ TTS Extension/background.js
@@ -1,4 +1,17 @@
-chrome.idle.setDetectionInterval(50); // 5 minutes
+chrome.action.onClicked.addListener(() => {
+    chrome.windows.getCurrent((window) => {
+      if (window) {
+        console.log("Current window ID:", window.id);
+        chrome.sidePanel.open({ windowId: window.id }).catch((error) => {
+          console.error("Failed to open side panel:", error);
+        });
+      } else {
+        console.error("No active window found.");
+      }
+    });
+  });
+
+chrome.idle.setDetectionInterval(15);  // 5 minutes, only accepts 15,30,60,300,600
 
 chrome.idle.onStateChanged.addListener((state) => {
     if (state === "idle" || state === "locked") {
